03dcd17d2ea953e15803be9b8a164794d5fe5a1d
Md Siddiqur Rahman <siddikcoder@gmail.com>
2021-12-23 23:40:15 +0600

    add react dnd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import Box from './Box';
 
 function App() {
+  const [index, setIndex] = useState(1);
+  function moveCard(i) { setIndex(i) }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box card={index === 1} moveCard={moveCard.bind(null, 1)}></Box>
+      <Box card={index === 2} moveCard={moveCard.bind(null, 2)}></Box>
+      <Box card={index === 3} moveCard={moveCard.bind(null, 3)}></Box>
     </div>
   );
 }
