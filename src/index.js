c7e10aa03bfa950de309dbc2007d01c8eefeb217
Md Siddiqur Rahman <siddikcoder@gmail.com>
2022-05-30 18:49:51 +0600

    update to react 18

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <DndProvider backend={HTML5Backend}>
       <App />
     </DndProvider>,
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
